b4c2d008156ac214ee61ee2922bd6464fb5d3338
vicky1408 <vigneshv01@gmail.com>
2019-01-30 10:52:07 +0530

    added database details

--- Data Setup/MySQL.sql
+++ Data Setup/MySQL.sql
@@ -1,2 +1,4 @@
+CREATE DATABASE OpenCRMDB;
+-- USE OpenCRMDB;
 Create Table customer(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(100), address varchar(255), PRIMARY KEY(id));
 INSERT INTO customer (name, address) values ("Madras Maligai", "8, Nehru Road, Tambaram West, Chennai 600045");
